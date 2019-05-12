c61e543fe68820a5875ca3c859e061b7e6231d01
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-12 23:14:24 +0300

    generating a test class

---
+++ Plocker_test.py
@@ -0,0 +1,10 @@
+from Plocker import User
+import unittest
+class UserTest(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
