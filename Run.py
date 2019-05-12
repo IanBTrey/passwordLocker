9a86ed9919f58a355843e29d588c5cc02282c58e
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-13 00:03:55 +0300

    added main functionality logic

--- Run.py
+++ Run.py
@@ -61,3 +61,108 @@ def random_password(limit):
         all=password[random.randint(0,run)]
         hold=hold+all
     return hold
+
+
+
+def main():
+        print("Hello! Welcome to your Password Locker.Sign up below")
+        name=input()
+        print("Use g to generate password or m to make your own password
+        short_codes=input().lower()
+        if short_codes=="g":
+            print("Enter the length that you would like your password to have. Recommended length:)5")
+            limit=int(input())
+            password=random_password(limit)
+            print("Your password is "+password)
+        else:
+            print("Enter your password")
+            password=input()
+        thisAccount="passwordLocker"
+        save_user(create(thisAccount,password,name))
+        print("\n")
+        print(f"Welcome {name}! Please Login below")
+        print("Enter the username that you just created")
+        User=input()
+        print("enter your password ")
+        Password=input()
+        if User==name and Password==password:
+            print("\n")
+            print(f"Welcome to your dashboard {name}")
+            while True:
+                print("These are short code to help you navigate through\n sp- save new password dp-- display all passwords fa-- find a specific account password  dp-- delete password ex-- exit this application")
+                short_codes=input().lower()
+                if short_codes=="sp":
+                    print("-"*20)
+                    print("Are you ready to create a new password?\n")
+                    print(f"{name} Please enter the account name you need to be saved eg-Instagram | Facebook | Github")
+                    this account=input()
+                    print("Enter username")
+                    username=input();
+                    print("Enter the password and make sure no one is watching")
+                    password=input()
+                    save_user(create(thisAccount,username,password))
+                    print("\n")
+                    print(f"{thisAccout} account details saved succefully")
+                    print("\n")
+
+                elif short_codes=="dp":
+                    if display_passwords():
+                        print(f"{name} These are all your passwords,make sure you keep them safe")
+                        print("--"*40)
+                        print("Account Name \t  Username\t password")
+                        print("_"*60)
+                        for account in display_passwords():
+                            print(f"{account.username} \t {account.password}............{account.email}")
+                            print("\n")
+                        print("--"*40)
+                    else:
+                        print("\n")
+                        print("It seems you have not Accounts yet")
+
+                elif short_codes=="fa":
+                    print("Enter Account name")
+                    search_pass=input();
+                    if check_account_existence(search_pass):
+                        searchP=search_account(search_pass)
+                        print("\n")
+                        print("Account Match ------1")
+                        print("Username\t password")
+                        print("__"*20)
+                        print(f"{searchP.password}...\t{searchP.email}")
+                        print("--"*20)
+                        print("\n")
+                    else:
+                        print("That Account does not exist Please Try again")
+                        print("--"*20)
+                        print("\n")
+
+                elif short_codes=="dp":
+                    print("Enter Account Name to be deleted")
+                    delAccount=input();
+                    if check_account_existence(delAccount):
+                        for i in display_passwords():
+                            if delAccount in i.username:
+                                posi=display_passwords().index(i)
+                        display_passwords().remove(display_passwords()[posi])
+                        print("\n")
+
+                        print(f"{delAccount} Account deleted succefully")
+                        print("\n")
+                    else:
+                        print("That acount does not exist")
+
+                elif short_codes=="ex":
+                    print("are you sure about this \n yes or no")
+                    sure=input().lower()
+                    if sure=="no":
+                        continue
+                    else:
+                        print("see yah later...Bye!!!")
+                        break
+                else:
+                    print("Invalid!...would you please use the short codes")
+        else:
+            print("wrong usermame or password")
+
+if __name__ == '__main__':
+    main()
