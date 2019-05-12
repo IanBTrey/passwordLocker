aee92c9f41fb74bb13c9f4b343706d1ee6639842
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-12 23:38:48 +0300

    generating init function

--- Plocker.py
+++ Plocker.py
@@ -3,3 +3,8 @@ class User:
     Class that generates new instances of the user.
     """
     user_details=[]; #empty user details
+
+    def __init__(self, username,password,email):
+        self.username=username
+        self.password=password
+        self.email=email
