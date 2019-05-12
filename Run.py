78728921d4678313eb5ef47bc606fb53dcc6f8e6
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-12 23:55:38 +0300

    added delete password and account function logic

--- Run.py
+++ Run.py
@@ -15,3 +15,17 @@ def save_user(Plocker):
     Function to save a new user
     '''
     Plocker.create()
+
+
+def delete_password(username,password,accountName):
+    '''
+    Function  to delete password
+    '''
+    user_details=User(username,password,email)
+    return user_details
+
+def delete_account(Plocker):
+    '''
+    Function to delete account
+    '''
+    Plocker.delete()
