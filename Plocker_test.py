2949b62e775588fa95235c79469d92d42446a1b1
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-12 23:18:39 +0300

    writing setup and teardown functions

--- Plocker_test.py
+++ Plocker_test.py
@@ -8,3 +8,13 @@ class UserTest(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+    #test 1
+    def tearDown(self):
+        '''
+        This function ensures that each time the test runs it runs afresh without memory of the previous details
+        '''
+        User.user_details=[]
+    def setUp(self):
+        """
+        This is a set up function that runs every time before each test clauses
+        """
