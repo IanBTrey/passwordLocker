65196fc07353f98daebcc19d910ee3c5b4839c8a
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-12 23:43:19 +0300

    added find account decorator

--- Plocker.py
+++ Plocker.py
@@ -13,3 +13,9 @@ class User:
         User.user_details.append(self)
     def delete(self):
         User.user_details.remove(self)
+
+    @classmethod
+    def find_by_accountName(cls,number):
+        for user in cls.user_details:
+            if user.username==number:
+                return user
